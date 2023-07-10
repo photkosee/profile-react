52e688e02dd181db2a178992d7abca8aed0c70fd
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-07-10 22:59:40 +1000

    sticky navbar

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -4,8 +4,7 @@ import "./project.css";
 const Project = () => {
     return (
         <section className="project section" id="project">
-            <h2 className="section__title">Projects</h2>
-            <span className="section__subtitle">open sources</span>
+            <h2 className="section__title project__name">Projects</h2>
             <div className="project__container container grid">
                 <div className="project__content">
                     <span>test</span>
