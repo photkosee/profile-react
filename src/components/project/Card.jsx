69c6c3712bbeb6ed44cad1d9e338991c70246c46
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-07-19 17:54:50 +1000

    responsive swiper

--- src/components/project/Card.jsx
+++ src/components/project/Card.jsx
@@ -1,139 +1,30 @@
 import React from "react";
 import "./card.css";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-// import required modules
-import { Pagination, Navigation } from 'swiper/modules';
-
-const Card = () => {
+const Card = (props) => {
+    const{ image, title, sub1, sub2, tools } = props;
     return (
-        <Swiper 
-            slidesPerView={4}
-            spaceBetween={30}
-            slidesPerGroup={4}
-            loop={true}
-            pagination={{
-                clickable: true,
-            }}
-            navigation={true}
-            modules={[Pagination, Navigation]}
-            className="slide-container"
-        >
-            <div className="slide-content">
-                <div className="card-wrapper">
-                    <SwiperSlide className="card">
-                        <div className="image-content">
-                            <div className="card-image">
-                                <img src="/profile_show.png" alt="Profile Website" className="card-img"></img>
-                            </div>
-                        </div>
-
-                        <div className="card-content">
-                            <h3 className="name">Profile Website</h3>
-                            <h3 className="subtitle">Personal Project <br/> Frontend Development</h3>
-                            <p className="tools">
-                                tools/version control:<br/>
-                                <img src="/js_logo.png" alt="JavaScript" className="card__icon"/>
-                                <img src="/html5_logo.png" alt="HTML5" className="card__icon"/>
-                                <img src="/css3_logo.png" alt="CSS3" className="card__icon"/>
-                                <img src="/react_logo.png" alt="React" className="card__icon"/>
-                                <img src="/github_logo.png" alt="Github" className="card__icon"/>
-                            </p>
-                            <button className="card-btn">More Details</button>
-                        </div>
-                    </SwiperSlide>
+        <div className="card-wrapper">
+            <div className="card">
+                <div className="image-content">
+                    <img src={image} alt="" className="card-img"></img>
                 </div>
-                <div className="card-wrapper">
-                    <SwiperSlide className="card">
-                        <div className="image-content">
-                            <div className="card-image">
-                                <img src="/dungeon_show.png" alt="Dungeon Mania" className="card-img"></img>
-                            </div>
-                        </div>
 
-                        <div className="card-content">
-                            <h3 className="name">Dungeon Mania</h3>
-                            <h3 className="subtitle">UNSW project <br/> Backend Development | OOP</h3>
-                            <p className="tools">
-                                tools/version control:<br/>
-                                <img src="/java_logo.png" alt="Java" className="card__icon"/>
-                                <img src="/gitlab_logo.png" alt="Gitlab" className="card__icon"/>
-                            </p>
-                            <button className="card-btn">More Details</button>
-                        </div>
-                    </SwiperSlide>
-                </div>
-                <div className="card-wrapper">
-                    <SwiperSlide className="card">
-                        <div className="image-content">
-                            <div className="card-image">
-                                <img src="/communication_show.png" alt="Communication Team" className="card-img"></img>
-                            </div>
-                        </div>
-
-                        <div className="card-content">
-                            <h3 className="name">Communication Team</h3>
-                            <h3 className="subtitle">UNSW project <br/> Backend Development</h3>
-                            <p className="tools">
-                                tools/version control:<br/>
-                                <img src="/js_logo.png" alt="JavaScript" className="card__icon"/>
-                                <img src="/ts_logo.png" alt="TypeScript" className="card__icon"/>
-                                <img src="/gitlab_logo.png" alt="Gitlab" className="card__icon"/>
-                            </p>
-                            <button className="card-btn">More Details</button>
-                        </div>
-                    </SwiperSlide>
-                </div>
-                <div className="card-wrapper">
-                    <SwiperSlide className="card">
-                        <div className="image-content">
-                            <div className="card-image">
-                                <img src="/communication_show.png" alt="Communication Team" className="card-img"></img>
-                            </div>
-                        </div>
-
-                        <div className="card-content">
-                            <h3 className="name">Communication Team</h3>
-                            <h3 className="subtitle">UNSW project <br/> Backend Development</h3>
-                            <p className="tools">
-                                tools/version control:<br/>
-                                <img src="/js_logo.png" alt="JavaScript" className="card__icon"/>
-                                <img src="/ts_logo.png" alt="TypeScript" className="card__icon"/>
-                                <img src="/gitlab_logo.png" alt="Gitlab" className="card__icon"/>
-                            </p>
-                            <button className="card-btn">More Details</button>
-                        </div>
-                    </SwiperSlide>
-                </div>
-                <div className="card-wrapper">
-                    <SwiperSlide className="card">
-                        <div className="image-content">
-                            <div className="card-image">
-                                <img src="/communication_show.png" alt="Communication Team" className="card-img"></img>
-                            </div>
-                        </div>
-
-                        <div className="card-content">
-                            <h3 className="name">Communication Team</h3>
-                            <h3 className="subtitle">UNSW project <br/> Backend Development</h3>
-                            <p className="tools">
-                                tools/version control:<br/>
-                                <img src="/js_logo.png" alt="JavaScript" className="card__icon"/>
-                                <img src="/ts_logo.png" alt="TypeScript" className="card__icon"/>
-                                <img src="/gitlab_logo.png" alt="Gitlab" className="card__icon"/>
-                            </p>
-                            <button className="card-btn">More Details</button>
-                        </div>
-                    </SwiperSlide>
+                <div className="card-content">
+                    <h3 className="name">{title}</h3>
+                    <h3 className="subtitle">{sub1} <br/> {sub2}</h3>
+                    <p className="tools">
+                        tools/version control:<br/>
+                        {tools.map((item) => {
+                            return (
+                                <img key={item} src={item} alt="" className="card__icon"/>
+                            )
+                        })}  
+                    </p>
+                    <button className="card-btn">More Details</button>
                 </div>
             </div>
-        </Swiper>
+        </div>
     );
 }
 
--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,13 +1,151 @@
 import React from "react";
 import "./project.css";
 import Card from "./Card";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
+const Projects = [
+    {
+        id: 1,
+        title: 'Profile Website',
+        image: '/profile_show.png',
+        sub1: 'Personal Project',
+        sub2: 'Frontend Development',
+        tools: [
+            '/js_logo.png',
+            '/html5_logo.png',
+            '/css3_logo.png',
+            '/react_logo.png',
+            '/github_logo.png'
+        ]
+    },
+    {
+        id: 2,
+        title: 'Dungeon Mania',
+        image: '/dungeon_show.png',
+        sub1: 'UNSW project',
+        sub2: 'Backend Development | OOP',
+        tools: [
+            '/java_logo.png',
+            '/gitlab_logo.png'
+        ]
+    },
+    {
+        id: 3,
+        title: 'Communication Team',
+        image: '/communication_show.png',
+        sub1: 'UNSW project',
+        sub2: 'Backend Development',
+        tools: [
+            '/js_logo.png',
+            '/ts_logo.png',
+            '/gitlab_logo.png',
+        ]
+    },
+    {
+        id: 4,
+        title: 'Blackout',
+        image: '/communication_show.png',
+        sub1: 'UNSW project',
+        sub2: 'Backend Development | OOP',
+        tools: [
+            '/java_logo.png',
+            '/gitlab_logo.png',
+        ]
+    },
+    {
+        id: 5,
+        title: 'File System',
+        image: '/communication_show.png',
+        sub1: 'UNSW project',
+        sub2: 'System calls implementation',
+        tools: [
+            '/c_logo.png',
+            '/gitlab_logo.png',
+        ]
+    },
+    {
+        id: 6,
+        title: 'Consumer & Producer',
+        image: '/communication_show.png',
+        sub1: 'UNSW project',
+        sub2: 'CLI Development',
+        tools: [
+            '/java_logo.png',
+            '/gitlab_logo.png',
+        ]
+    },
+    {
+        id: 7,
+        title: 'VM Fault Handling',
+        image: '/communication_show.png',
+        sub1: 'UNSW project',
+        sub2: 'HPT implementation',
+        tools: [
+            '/c_logo.png',
+            '/gitlab_logo.png',
+        ]
+    }
+];
 
 const Project = () => {
     return (
         <section className="project container" id="project">
             <div className="project__first">
-                <h2 className="section__title project__name">Projects</h2>
-                <Card />
+            <h2 className="section__title project__name">Projects</h2>
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    slidesPerGroup={1}
+                    loop={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    style={{
+                        "--swiper-navigation-color": "#dd7f7f",
+                        "--swiper-pagination-color": "#dd7f7f"
+                    }}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1
+                        },
+                        640: {
+                            slidesPerView: 2
+                        },
+                        900: {
+                            slidesPerView: 3
+                        },
+                      }}
+                    className={'swiper'}
+                >
+                    {Projects.map((item) => {
+                        return (
+                            <SwiperSlide
+                                key={item.id}
+                                className={'slide'}
+                            >
+                                <Card
+                                    key={item.id}
+                                    title={item.title}
+                                    image={item.image}
+                                    sub1={item.sub1}
+                                    sub2={item.sub2}
+                                    tools={item.tools}
+                                />
+                            </SwiperSlide>
+                        )
+                    })}
+                </Swiper>
             </div>
         </section>
     );
