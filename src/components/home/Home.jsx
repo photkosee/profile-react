19f66353eaad3e1c0f3f4e35f5d575e431815246
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-11-07 13:07:08 +1100

    update: resume

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -54,7 +54,7 @@ const Home = () => {
                     </a>
                 </div>
                 
-                <a href="https://drive.google.com/file/d/1vV-dBw1Wr0N64lAOhT5Ex2k_Upkk-zZP/view?usp=sharing" className="btn" target="{_blank}">
+                <a href="https://drive.google.com/file/d/1WG6orjyASFA57Z-bupVeYjr-wPb22MBM/view?usp=sharing" className="btn" target="{_blank}">
                     resume
                 </a>
             </div>
